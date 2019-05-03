13f50eb5c85a39e9756a0bdcca592bee8bfaed11
Guilherme Louro <guilherme-louro@hotmail.com>
2019-05-03 17:32:06 -0300

    Change lodash import

--- src/components/form/withContextForm.js
+++ src/components/form/withContextForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { get } from 'lodash';
+import get from 'lodash/get';
 import { Consumer } from './create';
 
 const withContextForm = WrapperComponent => {
