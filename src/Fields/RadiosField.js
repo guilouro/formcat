664db69af7986230bb919f3d29b2596a65ee3078
Guilherme Louro <guilherme-louro@hotmail.com>
2019-05-06 18:32:37 -0300

    Create RadiosField

---
+++ src/Fields/RadiosField.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import withContextForm from '../withContextForm'
+import Wrapper from './Wrapper'
+
+const RadiosField = ({ label, options, error, ...input }) => {
+  const [checkedItem, setCheckedItem] = useState(
+    options.find(item => item.checked).value
+  )
+
+  const handleClick = value => {
+    setCheckedItem(value)
+  }
+
+  return (
+    <>
+      {label && <div>{label}</div>}
+      {options.map(item => (
+        <Wrapper
+          key={item.value}
+          label={item.label}
+          error={error}
+          {...input}
+          reverse
+        >
+          <input
+            {...input}
+            value={item.value}
+            checked={item.value === checkedItem}
+            onClick={() => handleClick(item.value)}
+            type="radio"
+          />
+        </Wrapper>
+      ))}
+    </>
+  )
+}
+
+RadiosField.propTypes = {
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string,
+      value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+      checked: PropTypes.bool
+    })
+  ),
+  error: PropTypes.bool.isRequired,
+  label: PropTypes.string
+}
+
+RadiosField.defaultProps = {
+  label: '',
+  options: []
+}
+
+export default withContextForm(React.memo(RadiosField))
--- src/Fields/index.js
+++ src/Fields/index.js
@@ -2,7 +2,14 @@ import TextField from './TextField'
 import SelectField from './SelectField'
 import InputField from './InputField'
 import CheckboxField from './CheckboxField'
-import RadioField from './RadioField'
+import RadiosField from './RadiosField'
 import Submit from './Submit'
 
-export { TextField, SelectField, InputField, CheckboxField, RadioField, Submit }
+export {
+  TextField,
+  SelectField,
+  InputField,
+  CheckboxField,
+  RadiosField,
+  Submit
+}
